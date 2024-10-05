145403a6409daa83ae987a991b271df0248a4164
AdrielArgueta <158535336+AdrielArgueta@users.noreply.github.com>
2024-10-04 23:59:22 -0600

    clase recixer

--- BmpHandlerResizer.java
+++ BmpHandlerResizer.java
@@ -15,6 +15,73 @@ public class BmpHandlerResizer {
     private int[][] Azul;
 
     //constructor de la clase 
-    public BmpHandlerResizer(String archivo) 
+    public BmpHandlerResizer(String archivo) throws Exception{
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(archivo));
+        bis.head(header);
+        this.ancho = readAncho();
+        this.alto = readAlto();
+        this.Rojo = new int[this.alto][this.ancho];
+        this.Verde = new int[this.alto][this.ancho];
+        this.Azul = new int[this.alto][this.ancho];
+        readBmp(bis);
+        bis.close(); // cerramos el archivo  
+    }
+
+    //lecutra de la imagen 
+    private void readBmp(BufferedInputStream archivo) throws Exception{
+        for(int i = alto -1; i >= 0 ; i--){
+            for(int j = 0; j < ancho; j++){
+                int blue = archivo.read() & 0xFF;
+                int green = archivo.read()& 0xFF;
+                int red = archivo.read() & 0xFF;
+
+                Azul[i][j] = blue;
+                Verde[i][j]= green;
+                Rojo[i][j]= red;
+
+            }
+        }
+    }
+    //Metodo para reducir el ancho xd
+    public void reducirAncho(){
+        int nuevoAncho = ancho / 2;
+        int[][] nuevoRojo = new int[alto][nuevoAncho];
+        int[][] nuevoVerde = new int[alto][nuevoAncho];
+        int[][] nuevoAzul = new int[alto][nuevoAncho];
+
+        for(int i = 0; i<alto; i++){
+            for(int j = 0; j<ancho; j++){
+                nuevoRojo[i][j] = Rojo[i][j * 2];
+                nuevoVerde[i][j] = Verde[i][j * 2];
+                nuevoAzul[i][j] = Azul[i][j * 2];
+            }
+        }
+
+        this.ancho = nuevoAncho;
+        this.Rojo = nuevoRojo;
+        this.Verde = nuevoVerde;
+        this.Azul = nuevoAzul;
+    }
+
+    //metodo para reducir el alto xd 
+    public void reducirAlto(){
+        int nuevoAlto = alto /2;
+        int[][] nuevoRojo = new int[alto][nuevoAlto];
+        int[][] nuevoVerde = new int[alto][nuevoAlto];
+        int[][] nuevoAzul = new int[alto][nuevoAlto];
+
+        for(int i = 0; i < nuevoAlto; i++){
+            for(int j = 0; j < ancho; i++){
+                nuevoRojo[i][j] = Rojo[i * 2][j];
+                nuevoVerde[i][j] = Verde[i * 2][j];
+                nuevoAzul[i][j] = Azul [i * 2][j];
+            }
+        }
+
+        this.alto = nuevoAlto;
+        this.Rojo = nuevoRojo;
+        this.Azul = nuevoAzul;
+        this.Verde = nuevoVerde;
+    }
 
 }
